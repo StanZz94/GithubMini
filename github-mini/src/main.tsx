302c6ded279cbc734bbe332544e6245cc1b70c97
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-02-24 22:55:57 +0200

    setup react query

--- github-mini/src/main.tsx
+++ github-mini/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+<QueryClientProvider client={queryClient}>
+  <App />
+</QueryClientProvider>
+
