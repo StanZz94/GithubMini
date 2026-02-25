02f4771d72415ff4a4362d5cc8f4b26320c6ac0d
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-02-25 04:41:19 +0200

    setup routes

--- github-mini/src/main.tsx
+++ github-mini/src/main.tsx
@@ -1,11 +1,16 @@
-import './index.css'
-import App from './App.tsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./router";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-<QueryClientProvider client={queryClient}>
-   <App />
-</QueryClientProvider>
-
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ github-mini/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="text-center space-y-6">
+      <h1 className="text-4xl font-bold">
+        GitHub Developer Analytics
+      </h1>
+
+      <p className="text-gray-600 max-w-2xl mx-auto">
+        Search for any GitHub user and explore their repositories,
+        statistics, language distribution and performance insights.
+      </p>
+    </div>
+  );
+}
