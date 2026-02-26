8fbed1b9a8f6afbf67814d10cb1ea61db4fbef69
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-02-26 02:23:55 +0200

    style navbar

--- github-mini/src/components/layout/Navbar.tsx
+++ github-mini/src/components/layout/Navbar.tsx
@@ -2,17 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header className="border-b bg-white">
-      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+    <header className="border-b border-gray-500 bg-stone-800">
+      <div className="max-w-6xl mx-auto px-6 py-3 flex justify-between items-center">
         <Link
           to="/"
-          className="text-xl font-bold tracking-tight hover:opacity-80 transition"
+          className="hover:opacity-80 transition"
         >
-          Github-Mini
+          <img
+            src="/gitlogo.png"
+            alt="GitHub Mini Logo"
+            className="w-16 h-16 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
+          />
         </Link>
 
-        <nav className="flex gap-6 text-sm font-medium text-gray-600">
-          <Link to="/" className="hover:text-black transition">
+        <nav className="flex gap-6 text-sm font-medium text-gray-400">
+          <Link to="/" className="hover:text-white transition">
             Home
           </Link>
         </nav>
