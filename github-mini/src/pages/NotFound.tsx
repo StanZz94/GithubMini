6d65ffcdc74ec8449a58985873d8f3a4842532a9
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-02-28 23:13:03 +0200

    add error page to router and add some styles

--- github-mini/src/pages/NotFound.tsx
+++ github-mini/src/pages/NotFound.tsx
@@ -4,10 +4,10 @@ export default function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-6 text-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-stone-800 px-6 text-center">
       
       {/* 404 Image */}
-      <div className="max-w-md mb-8">
+      <div className="w-80 mb-4">
         <img
           src="/404decoration.png"
           alt="404 Not Found"
@@ -16,18 +16,14 @@ export default function NotFound() {
       </div>
 
       {/* Text */}
-      <h1 className="text-5xl font-bold text-gray-800 mb-4">
-        404
-      </h1>
-
-      <p className="text-lg text-gray-600 mb-8 max-w-md">
+      <p className="text-2xl font-semibold text-gray-200 mb-10 max-w-md">
         Oops! The page you’re looking for doesn’t exist or may have been moved.
       </p>
 
       {/* Back Button */}
       <button
         onClick={() => navigate("/")}
-        className="px-6 py-3 bg-black text-white rounded-xl font-semibold shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300"
+        className="px-6 py-3 bg-gray-200 border-2 border-gray-400 text-black rounded-xl font-semibold shadow-md hover:bg-gray-300 hover:border-white hover:drop-shadow-[0_0_20px_#ffffff] transition-all duration-300"
       >
         ← Back to Dashboard
       </button>
--- github-mini/src/router.tsx
+++ github-mini/src/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./components/layout/RootLayout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
