62c0b8324a80ba9588523319fbdfb43535290b66
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-01 08:20:29 +0200

    add fallback if no data in start chart

--- github-mini/src/components/profile/StarsBarChart.tsx
+++ github-mini/src/components/profile/StarsBarChart.tsx
@@ -13,19 +13,43 @@ interface Props {
 }
 
 export default function StarsBarChart({ repos }: Props) {
+  if (!repos.length) return null;
+
   const topRepos = [...repos]
-    .sort(
-      (a, b) =>
-        b.stargazers_count - a.stargazers_count
-    )
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
     .slice(0, 5)
     .map((repo) => ({
       name: repo.name,
       stars: repo.stargazers_count,
     }));
 
+  // 🔥 Check if all stars are 0
+  const allZero = topRepos.every((repo) => repo.stars === 0);
+
+  if (allZero) {
+    return (
+      <div className="px-6 py-4 bg-gray-200 rounded-2xl text-center">
+        <h3 className="text-lg font-semibold mb-12">
+          Top Starred Repositories
+        </h3>
+
+        <div className="w-40 h-auto mx-auto">
+          <img
+            src="/noData.png"
+            alt="No data"
+            className="mx-auto w-full h-auto"
+          />
+        </div>
+
+        <p className="text-gray-700 font-semibold text-xl max-w-sm mx-auto">
+          No stars yet!
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="bg-white p-6 rounded-2xl shadow">
+    <div className="bg-gray-200 p-6 rounded-2xl shadow">
       <h3 className="text-lg font-semibold mb-4 text-center">
         Top Starred Repositories
       </h3>
@@ -36,7 +60,7 @@ export default function StarsBarChart({ repos }: Props) {
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
-            <Bar dataKey="stars" />
+            <Bar dataKey="stars" fill="#f59e0b" />
           </BarChart>
         </ResponsiveContainer>
       </div>
