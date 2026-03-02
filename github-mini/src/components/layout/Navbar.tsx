f670112668b2a8ecbc49f7d3d054a2dc533cc4b9
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-02 04:09:38 +0200

    add decorative button to nav

--- github-mini/src/components/layout/Navbar.tsx
+++ github-mini/src/components/layout/Navbar.tsx
@@ -4,25 +4,29 @@ export default function Navbar() {
   return (
     <header className="border-b-2 bg-stone-800">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        <Link
-          to="/"
-          className="hover:opacity-80 transition"
-        >
+        
           <img
             src="/gitlogo.png"
             alt="GitHub Mini Logo"
-            className="w-16 h-16 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
+            className="w-18 h-18 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
           />
-        </Link>
+    
 
         <h1 className="text-3xl font-extrabold text-gray-400 font-serif">
           GitHub Developer Analytics
         </h1>
 
         <nav className="flex gap-6 text-lg font-semibold text-gray-400">
-          <Link to="/" className="hover:text-white transition hover:drop-shadow-[0_0_25px_#ffffff]">
-            Dashboard
-          </Link>
+          <Link
+          to="/"
+          className=""
+        >
+          <img
+            src="/back2.png"
+            alt="Back Icon"
+            className="w-21 h-17 inline-block mr-2 drop-shadow-[0_0_5px_#ffffff] hover:drop-shadow-[0_0_15px_#ffffff]"
+          />
+        </Link>
         </nav>
       </div>
     </header>
