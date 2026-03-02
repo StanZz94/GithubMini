ef071b94a0f22939433b27e73b5fec8564bf7a22
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-02 04:13:18 +0200

    hide button back in "/"

--- github-mini/src/components/layout/Navbar.tsx
+++ github-mini/src/components/layout/Navbar.tsx
@@ -1,32 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
+
   return (
     <header className="border-b-2 bg-stone-800">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         
+        {/* LEFT SIDE */}
+        <div className="flex items-center">
           <img
             src="/gitlogo.png"
             alt="GitHub Mini Logo"
             className="w-18 h-18 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
           />
-    
+        </div>
 
+        {/* TITLE */}
         <h1 className="text-3xl font-extrabold text-gray-400 font-serif">
           GitHub Developer Analytics
         </h1>
 
+        {/* RIGHT SIDE */}
         <nav className="flex gap-6 text-lg font-semibold text-gray-400">
-          <Link
-          to="/"
-          className=""
-        >
-          <img
-            src="/back2.png"
-            alt="Back Icon"
-            className="w-21 h-17 inline-block mr-2 drop-shadow-[0_0_5px_#ffffff] hover:drop-shadow-[0_0_15px_#ffffff]"
-          />
-        </Link>
+          {!isHome && (
+            <Link to="/">
+              <img
+                src="/back2.png"
+                alt="Back Icon"
+                className="w-21 h-17 inline-block drop-shadow-[0_0_5px_#ffffff] hover:drop-shadow-[0_0_15px_#ffffff] transition"
+              />
+            </Link>
+          )}
         </nav>
       </div>
     </header>
