6d8861d3fb99a9fbde114acaa14e6e6f6fbf0bc5
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-03 09:45:05 +0200

    preload needed images

--- github-mini/src/components/layout/RootLayout.tsx
+++ github-mini/src/components/layout/RootLayout.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import { useImagePreload } from "../../hooks/useImagePreload";
 
 export default function RootLayout() {
+
+  const imagesLoaded = useImagePreload([
+    "/gitlogo.png",
+    "/back2.png",
+    "/octocat.png",
+  ]);
+
+  if (!imagesLoaded) {
+    return (
+      <div className="h-screen flex items-center justify-center bg-black text-white">
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen flex flex-col bg-stone-700">
       <Navbar />
-      
+
       <main className="flex-1 max-w-6xl w-full mx-auto px-6 py-10">
         <Outlet />
       </main>
