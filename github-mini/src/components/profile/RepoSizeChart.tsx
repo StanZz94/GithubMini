16253a5317a449fbb9f1dc07ccbf4dcb946908ac
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-04 20:35:15 +0200

    add fallback if no data to repoSize chart

--- github-mini/src/components/profile/RepoSizeChart.tsx
+++ github-mini/src/components/profile/RepoSizeChart.tsx
@@ -13,20 +13,44 @@ interface Props {
 }
 
 export default function RepoSizeChart({ repos }: Props) {
-  if (!repos.length) return null;
+  const hasRepos = repos.length > 100000;
 
-  const data = repos
-    .slice()
-    .sort((a, b) => b.size - a.size)
-    .slice(0, 10)
-    .map((repo) => ({
-      name: repo.name,
-      size: repo.size, // size in KB
-    }));
+  const data = hasRepos
+    ? repos
+        .slice()
+        .sort((a, b) => b.size - a.size)
+        .slice(0, 10)
+        .map((repo) => ({
+          name: repo.name,
+          size: repo.size, // KB
+        }))
+    : [];
+
+  if (!hasRepos || data.length === 0) {
+    return (
+      <div className="bg-gray-200 px-6 py-4 rounded-2xl shadow text-center">
+        <h3 className="text-2xl text-stone-700 font-semibold mb-12">
+          Repository Size (Top 10)
+        </h3>
+
+        <div className="w-40 mx-auto">
+          <img
+            src="/noData.png"
+            alt="No data"
+            className="w-full h-auto"
+          />
+        </div>
+
+        <p className="text-gray-700 font-semibold text-xl max-w-sm mx-auto">
+          No repository size data available.
+        </p>
+      </div>
+    );
+  }
 
   return (
-    <div className="bg-white p-6 rounded-2xl shadow">
-      <h3 className="text-xl font-bold mb-4 text-center">
+    <div className="bg-gray-200 px-6 py-4 rounded-2xl shadow">
+      <h3 className="text-2xl text-stone-700 font-semibold mb-12 text-center">
         Repository Size (Top 10)
       </h3>
 
