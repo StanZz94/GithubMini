9a7579de46d6598e5f2dcd32087df044b790542a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-10 01:30:42 +0200

    mobile styles for nav

--- github-mini/src/components/layout/Navbar.tsx
+++ github-mini/src/components/layout/Navbar.tsx
@@ -14,12 +14,12 @@ export default function Navbar() {
           <img
             src="/gitlogo.png"
             alt="GitHub Mini Logo"
-            className="w-18 h-18 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
+            className="w-12 h-12 md:w-18 md:h-18 inline-block mr-2 drop-shadow-[0_0_20px_#ffffff]"
           />
         </div>
 
         {/* TITLE */}
-        <h1 className="text-3xl font-extrabold text-gray-400 font-serif">
+        <h1 className="hidden md:block text-3xl font-extrabold text-gray-400 font-serif">
           GitHub Developer Analytics
         </h1>
 
@@ -30,12 +30,15 @@ export default function Navbar() {
               <img
                 src="/back2.png"
                 alt="Back Icon"
-                className="w-21 h-17 inline-block drop-shadow-[0_0_5px_#ffffff] hover:drop-shadow-[0_0_15px_#ffffff] transition"
+                className="w-15 h-12 md:w-21 md:h-17 inline-block drop-shadow-[0_0_5px_#ffffff] hover:drop-shadow-[0_0_15px_#ffffff] transition"
               />
             </Link>
           )}
         </nav>
       </div>
+      <h1 className="md:hidden text-base pb-2 font-extrabold text-gray-400 text-center font-serif">
+          GitHub Developer Analytics
+        </h1>
     </header>
   );
 }
