dc25b75e468c63c644d09db8adf43756f3437535
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-11 20:22:55 +0200

    profile mobile styles

--- github-mini/src/pages/Profile.tsx
+++ github-mini/src/pages/Profile.tsx
@@ -36,13 +36,13 @@ export default function Profile() {
   return (
     <div className="max-w-6xl mx-auto">
       {/* HEADER CARD */}
-      <div className="bg-gray-200 rounded-2xl pt-8 px-8">
-        <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
+      <div className="bg-gray-200 rounded-2xl pt-4 md:pt-8 px-4 md:px-8">
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4 md:gap-8">
           {/* Avatar */}
           <img
             src={user.avatar_url}
             alt={user.login}
-            className="w-32 h-32 rounded-full border"
+            className="w-36 h-36 rounded-full border"
           />
 
           {/* User Info */}
