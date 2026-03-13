1ec1d03b06f3af78003f9dafc97bc1a4daf04853
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-13 02:40:28 +0200

    some styles for footer

--- github-mini/src/components/layout/Footer.tsx
+++ github-mini/src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="border-t-2 bg-stone-800 mt-10">
-      <div className="max-w-6xl mx-auto px-6 py-6 text-base text-gray-400 text-center">
-        © {new Date().getFullYear()} Github Developer Analytics. Built with React & GitHub API.
+    <footer className="border-t-2 bg-stone-800 mt-6 md:mt-10">
+      <div className="max-w-6xl mx-auto px-6 py-3 md:py-6 text-sm md:text-base text-gray-400 text-center">
+        © {new Date().getFullYear()} Github Developer Analytics. Built with ReactJS & GitHub API.
       </div>
     </footer>
   );
