c36a80340c4d25e0ee4d22b5c72da0b6c53a5657
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-13 02:38:17 +0200

    mobile friendly comparison chart

--- github-mini/src/components/profile/PopularityComparisonChart.tsx
+++ github-mini/src/components/profile/PopularityComparisonChart.tsx
@@ -26,7 +26,6 @@ export default function PopularityComparisonChart({ repos }: Props) {
       forks: repo.forks_count,
     }));
 
-  // 🔥 Check if everything is 0
   const allZero = data.every((repo) => repo.stars === 0 && repo.forks === 0);
 
   if (allZero) {
@@ -43,6 +42,7 @@ export default function PopularityComparisonChart({ repos }: Props) {
             className="mx-auto w-full h-auto"
           />
         </div>
+
         <p className="text-gray-700 font-semibold text-xl max-w-sm mx-auto">
           No stars or forks yet!
         </p>
@@ -51,20 +51,47 @@ export default function PopularityComparisonChart({ repos }: Props) {
   }
 
   return (
-    <div className="bg-gray-200 px-6 py-4 rounded-2xl shadow">
-      <h3 className="text-2xl text-stone-700 font-semibold mb-4 text-center">
+    <div className="bg-gray-200 px-4 md:px-6 py-4 rounded-2xl shadow">
+      <h3 className="text-xl md:text-2xl text-stone-700 font-semibold mb-4 text-center">
         Stars vs Forks (Top Repos)
       </h3>
 
-      <div className="w-full h-64">
+      <div className="w-full h-64 md:h-70">
         <ResponsiveContainer width="100%" height="100%">
-          <BarChart data={data}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 10,
+              left: 5,
+              bottom: 0,
+            }}
+          >
             <XAxis dataKey="name" hide />
-            <YAxis />
+
+            <YAxis
+              width="auto"
+              tick={{ fontSize: 12 }}
+              allowDecimals={false}
+            />
+
             <Tooltip />
+
             <Legend />
-            <Bar dataKey="stars" fill="#f59e0b" />
-            <Bar dataKey="forks" fill="#10b981" />
+
+            <Bar
+              dataKey="stars"
+              fill="#f59e0b"
+              radius={[6, 6, 0, 0]}
+              maxBarSize={40}
+            />
+
+            <Bar
+              dataKey="forks"
+              fill="#10b981"
+              radius={[6, 6, 0, 0]}
+              maxBarSize={40}
+            />
           </BarChart>
         </ResponsiveContainer>
       </div>
