ff82f551a75c1062034b26b7cffc4b1a0bd29a32
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2026-03-18 04:38:42 +0200

    refactor repoSize component

--- github-mini/src/components/profile/RepoSizeChart.tsx
+++ github-mini/src/components/profile/RepoSizeChart.tsx
@@ -8,50 +8,10 @@ import {
 } from "recharts";
 import { useState, useRef, useEffect } from "react";
 import type { GithubRepo } from "../../types/github";
+import CustomTooltip from "./CustomTooltip";
 
 interface Props {
   repos: GithubRepo[];
-}
-
-// ================= CUSTOM TOOLTIP =================
-interface CustomTooltipProps {
-  active?: boolean;
-  // eslint-disable-next-line
-  payload?: any;
-  label?: string;
-  coordinate?: { x: number; y: number };
-}
-
-function CustomTooltip({
-  active,
-  payload,
-  label,
-  coordinate,
-}: CustomTooltipProps) {
-  if (!active || !payload || payload.length === 0) return null;
-
-  return (
-    <div
-      className="bg-white border p-2 rounded shadow text-sm"
-      style={{
-        position: "absolute",
-        left: coordinate?.x,
-        top: coordinate?.y,
-        pointerEvents: "none",
-        transform: "translate(-50%, -100%)",
-        whiteSpace: "nowrap",
-        zIndex: 1000,
-      }}
-    >
-      <p className="font-semibold">{label}</p>
-      {/* eslint-disable-next-line */}
-      {payload.map((item: any) => (
-        <p key={item.dataKey} className="text-gray-700">
-          {item.name}: {item.value} KB
-        </p>
-      ))}
-    </div>
-  );
 }
 
 export default function RepoSizeChart({ repos }: Props) {
